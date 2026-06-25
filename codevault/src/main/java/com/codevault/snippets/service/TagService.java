629c2b3241ce0c977bb4a873d2d8a425792d887b
Krisha Shah <krishashah3011@gmail.com>
2026-06-25 21:47:17 +0530

    Delete codevault directory

--- codevault/src/main/java/com/codevault/snippets/service/TagService.java
+++
@@ -1,9 +0,0 @@
-package com.codevault.snippets.service;
-
-import com.codevault.snippets.entity.Tag;
-
-public interface TagService {
-
-    Tag getOrCreateTag(String name);
-    Tag findOrCreate(String name);
-}
